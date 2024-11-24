8a4a88bc1ec10edd695bfe71259e367558025551
yadamoon <yared241993@gmail.com>
2024-11-24 14:06:55 +0300

    i created and completed partner component

---
+++ src/app/portal/partner/partner.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-partner',
+  standalone: true,
+  imports: [],
+  templateUrl: './partner.component.html',
+  styleUrl: './partner.component.css'
+})
+export class PartnerComponent {
+
+}

6b090206b22b2522330178c5071f5f44014827ad
yadamoon <yared241993@gmail.com>
2024-11-24 14:08:40 +0300

    i created and completed header component

--- src/app/portal/common/header/header.component.ts
+++ src/app/portal/common/header/header.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BannerComponent } from '../../banner/banner.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, BannerComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isBrowseOpen = false;
+  isLoginOpen = false;
+
+  toggleBrowse() {
+    this.isBrowseOpen = !this.isBrowseOpen;
+    this.isLoginOpen = false;
+  }
+
+  toggleLogin() {
+    this.isLoginOpen = !this.isLoginOpen;
+    this.isBrowseOpen = false;
+  }
 
 }

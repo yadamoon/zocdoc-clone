6522cfe68b9cd3758753e8af5ed34203e1ac0c4b
yadamoon <yared241993@gmail.com>
2024-11-26 10:47:35 +0300

    completed

--- src/app/portal/common/footer/footer.component.ts
+++ src/app/portal/common/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { FlowbiteService } from '../../../services/flowbite.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) { }
 
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      console.log('Flowbite loaded', flowbite);
+    });
+  }
 }

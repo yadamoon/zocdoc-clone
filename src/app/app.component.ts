0557a97e7c97ae9b440be3207c55682a53dc0ea8
yadamoon <yared241993@gmail.com>
2024-11-26 10:48:31 +0300

    updated sth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { NetworkDoctorComponent } from './portal/network-doctor/network-doctor.component';
+import { Component, OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { HeaderComponent } from './portal/common/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { ServicesComponent } from './portal/services/services.component';
+import { AdvertComponent } from './portal/advert/advert.component';
+import { PartnerComponent } from './portal/partner/partner.component';
+import { DoctorsComponent } from './portal/doctors/doctors.component';
 import { FooterComponent } from './portal/common/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, NetworkDoctorComponent, ServicesComponent, AdvertComponent, PartnerComponent, DoctorsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'zocdoc-clone';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/app/portal/common/header/header.component.ts
+++ src/app/portal/common/header/header.component.ts
@@ -12,10 +12,213 @@ import { BannerComponent } from '../../banner/banner.component';
 export class HeaderComponent {
   isBrowseOpen = false;
   isLoginOpen = false;
+  selectedSpeciality: string | null = null;
+  selectedTab = 'doctors';
+
+  doctorSpecialities = [
+    'Primary Care Physicians',
+    'Psychiatrists',
+    'Optometrists',
+    'Dermatologists',
+    'Dentists',
+    'Therapist-Counselors',
+    'Ophthalmologists',
+    'OBGYNs',
+    'Psychologists',
+    'Chiropractors',
+    'Pediatricians',
+    'Orthopedic Surgeons',
+  ];
+  facilitySpecialities = [
+    'Urgent Care',
+    'Podiatrists',
+  ];
+  commonVisitReasons = [{
+    name: 'Dentists',
+    subCommonVisitReasons: [
+      'Medical Doctors',
+      'Medical Dentists',
+      'Medical Pediatric Dentists',
+      'Medical Pediatricians',
+      'Medical Dermatologists',
+      'Medical OB-GYNs',
+      'Medical Psychiatrists',
+      'Medical Therapists',
+      'Medical Eye Doctors',
+      'Medical ENT Doctors',
+      'Medical Urgent Care',
+      'Medical Medicare'
+    ], showSubCities: false
+  },
+  {
+    name: 'OBGYNs', subCommonVisitReasons: [
+      'Dental cleaning',
+      'Teeth bonding',
+      'Dental implants'
+    ]
+  },
+  {
+    name: 'Psychologists', subCommonVisitReasons: [
+      'Annual eye exam',
+      'Eye exam'
+    ], showSubCities: false
+  },
+  {
+    name: 'Psychiatrists',
+    subCommonVisitReasons: [
+      'Medical Doctors',
+      'Medical Dentists',
+      'Medical Pediatric Dentists',
+      'Medical Pediatricians',
+      'Medical Dermatologists',
+      'Medical OB-GYNs',
+      'Medical Psychiatrists',
+      'Medical Therapists',
+      'Medical Eye Doctors',
+      'Medical ENT Doctors',
+      'Medical Urgent Care',
+      'Medical Medicare'
+    ], showSubCities: false
+  },
+  {
+    name: 'Therapist - Counselors', subCommonVisitReasons: [
+      'Dental cleaning',
+      'Teeth bonding',
+      'Dental implants'
+    ]
+  },
+  {
+    name: 'Urgent Care', subCommonVisitReasons: [
+      'Annual eye exam',
+      'Eye exam'
+    ], showSubCities: false
+  },
+  {
+    name: 'Chiropractors',
+    subCommonVisitReasons: [
+      'Medical Doctors',
+      'Medical Dentists',
+      'Medical Pediatric Dentists',
+      'Medical Pediatricians',
+      'Medical Dermatologists',
+      'Medical OB-GYNs',
+      'Medical Psychiatrists',
+      'Medical Therapists',
+      'Medical Eye Doctors',
+      'Medical ENT Doctors',
+      'Medical Urgent Care',
+      'Medical Medicare'
+    ], showSubCities: false
+  },
+  {
+    name: 'Optometrists', subCommonVisitReasons: [
+      'Dental cleaning',
+      'Teeth bonding',
+      'Dental implants'
+    ]
+  },
+  {
+    name: 'Ophthalmologists', subCommonVisitReasons: [
+      'Annual eye exam',
+      'Eye exam'
+    ], showSubCities: false
+  },
+  {
+    name: 'Podiatrists',
+    subCommonVisitReasons: [
+      'Medical Doctors',
+      'Medical Dentists',
+      'Medical Pediatric Dentists',
+      'Medical Pediatricians',
+      'Medical Dermatologists',
+      'Medical OB-GYNs',
+      'Medical Psychiatrists',
+      'Medical Therapists',
+      'Medical Eye Doctors',
+      'Medical ENT Doctors',
+      'Medical Urgent Care',
+      'Medical Medicare'
+    ], showSubCities: false
+  },
+  {
+    name: 'Pediatricians', subCommonVisitReasons: [
+      'Dental cleaning',
+      'Teeth bonding',
+      'Dental implants'
+    ]
+  },
+  {
+    name: 'Dermatologists', subCommonVisitReasons: [
+      'Annual eye exam',
+      'Eye exam'
+    ], showSubCities: false
+  },
+  {
+    name: 'Orthopedic Surgeons',
+    subCommonVisitReasons: [
+      'Medical Doctors',
+      'Medical Dentists',
+      'Medical Pediatric Dentists',
+      'Medical Pediatricians',
+      'Medical Dermatologists',
+      'Medical OB-GYNs',
+      'Medical Psychiatrists',
+      'Medical Therapists',
+      'Medical Eye Doctors',
+      'Medical ENT Doctors',
+      'Medical Urgent Care',
+      'Medical Medicare'
+    ], showSubCities: false
+  },
+  {
+    name: 'Procedures', subCommonVisitReasons: [
+      'Dental cleaning',
+      'Teeth bonding',
+      'Dental implants'
+    ]
+  }
+  ]
+
+  producer = {
+    name: 'producer',
+    subCityProducer: [
+      {
+        name: 'City 1',
+        showSubCities: false,
+        services: [
+          "Hyperactive Disorder (ADD / ADHD)",
+          "OB / GYN Emergency",
+          "Nexplanon Removal"
+        ]
+      },
+      {
+        name: 'City 2',
+        showSubCities: false,
+        services: [
+          "Pre-Surgery Checkup / Pre-Surgical Clearance",
+          "Annual Physical",
+          "LGBT Care"
+        ]
+      },
+      {
+        name: 'City 3',
+        showSubCities: false,
+        services: [
+          "ENT Emergency Visit",
+          "Pain Medication Prescription",
+          "IUD Insertion"
+        ]
+      }
+      // Add more cities as needed
+    ]
+  };
+
+
 
   toggleBrowse() {
     this.isBrowseOpen = !this.isBrowseOpen;
     this.isLoginOpen = false;
+
   }
 
   toggleLogin() {
@@ -23,4 +226,21 @@ export class HeaderComponent {
     this.isBrowseOpen = false;
   }
 
+
+  toggleModal() {
+    this.isBrowseOpen = !this.isBrowseOpen;
+  }
+
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+  }
+
+  selectSpeciality(speciality: string) {
+    this.selectedSpeciality = speciality;
+    this.toggleBrowse();
+  }
+
+  toggleSubCity(city: any) {
+    city.showSubCities = !city.showSubCities;
+  }
 }

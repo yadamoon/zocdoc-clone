4f8b8ea31e48c045c9ae314f51a23fb5aa0f2e2f
yadamoon <yared241993@gmail.com>
2024-12-01 19:21:57 +0300

    implmented isHomePage , isProviderPage

--- src/app/portal/components/layout/layout.component.ts
+++ src/app/portal/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../common/header/header.component';
@@ -7,18 +7,29 @@ import { ServicesComponent } from '../services/services.component';
 import { AdvertComponent } from '../advert/advert.component';
 import { PartnerComponent } from '../partner/partner.component';
 import { DoctorsComponent } from '../doctors/doctors.component';
-import { SignInComponent } from '../../../auth/sign-in/sign-in.component';
-import { BannerComponent } from '../banner/banner.component';
 import { FooterComponent } from '../common/footer/footer.component';
+import { BannerComponent } from '../banner/banner.component';
+import { SignInComponent } from '../../../auth/components/sign-in/sign-in.component';
 
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, HeaderComponent, NetworkDoctorComponent, ServicesComponent, AdvertComponent, PartnerComponent, DoctorsComponent, SignInComponent, BannerComponent, FooterComponent],
+  imports: [RouterOutlet, CommonModule, HeaderComponent, NetworkDoctorComponent, ServicesComponent, AdvertComponent, PartnerComponent, DoctorsComponent, SignInComponent, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
+  constructor(private router: Router) { }
+  isHomePage(): boolean {
+    return this.router.url === '/' || this.router.url === '/portal';
+  }
 
+  isProviderPage(): boolean {
+    return this.router.url === '/providers';
+  }
+
+  isHelpPage(): boolean {
+    return this.router.url === '/help';
+  }
 }
